22d9ce612d471f65b5eebaa05ec94cb388da8a2a
Rony Muliana <rony.muliana@gmail.com>
2020-01-20 20:55:30 +0700

    Beginning Javascript

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,20 @@
+// function Person(fullname, favColor) {
+//     this.name = fullname;
+//     this.favoriteColor = favColor;
+//     this.greet = function () {
+//         console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".")
+//     }
+// }
+
+class Person {
+    constructor(name, favoriteColor) {
+        this.name = name;
+        this.favoriteColor = favoriteColor;
+    }
+
+    greet() {
+        console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".")
+    }
+}
+
+export default Person;
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const postCSSPlugins = [
     require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
+    require('postcss-hexrgba'),
     require('autoprefixer'),
 ]
 
@@ -15,7 +16,7 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     devServer: {
-        before: function(app, server) {
+        before: function (app, server) {
             server._watch('./app/**/*.html')
         },
         contentBase: path.join(__dirname, 'app'),
@@ -29,7 +30,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css/i,
-                use: ['style-loader','css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+                use: ['style-loader', 'css-loader', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }]
             }
         ]
     }
